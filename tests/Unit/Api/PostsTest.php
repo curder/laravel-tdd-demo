b73b29bfee1fa2a145262a641fa8184231aef094
curder <curder@foxmail.com>
2019-09-26 14:52:07 +0800

    编写修改测试

--- tests/Unit/Api/PostsTest.php
+++ tests/Unit/Api/PostsTest.php
@@ -35,4 +35,19 @@ class PostsTest extends TestCase
             ->assertStatus(200)
             ->assertJson($post->toArray());
     }
+
+
+    /**
+     * @test
+     */
+    public function it_can_update_a_post()
+    {
+        $post = factory(Post::class)->create();
+
+        $updated_data = factory(Post::class)->make();
+
+        $this->patch(route('api.posts.update', $post), $updated_data)
+            ->assertStatus(200)
+            ->assertJson($updated_data->toArray());
+    }
 }
