9d92fb68faabf4efe586a559911f0fd03667ad61
curder <curder@foxmail.com>
2019-09-26 13:13:50 +0800

    修改测试基类

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,35 @@
 
 namespace Tests;
 
+use Faker\Factory as Faker;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, DatabaseMigrations, DatabaseTransactions;
+
+    /**
+     * @var Faker
+     */
+    protected $faker;
+
+    /**
+     * Set up the test
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->faker = Faker::create();
+    }
+
+    /**
+     * Reset the migrations
+     */
+    protected function tearDown(): void
+    {
+        $this->artisan('migrate:reset');
+        parent::tearDown();
+    }
 }
