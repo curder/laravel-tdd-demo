5fb7cf422ac34d72b0b69939ed57deb9cd878c88
curder <curder@foxmail.com>
2019-09-26 13:15:31 +0800

    添加路由

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,8 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'v1', 'namespace' => 'Api', 'as' => 'api.'], function () {
+    Route::post('posts', 'PostsController@store')->name('posts.store'); // 保存文章
 });
